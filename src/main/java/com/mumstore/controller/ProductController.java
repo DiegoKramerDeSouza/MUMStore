98747750874b20e4af9d5d18c1f5fcbef3b812e2
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-06-15 17:04:46 -0500

    out and target deleted

--- src/main/java/com/mumstore/controller/ProductController.java
+++ src/main/java/com/mumstore/controller/ProductController.java
@@ -33,6 +33,7 @@ public class ProductController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
         PrintWriter out = resp.getWriter();
         MongoDatabase con = (MongoDatabase) sc.getAttribute("dbdatabase");
         if(req.getParameter("id") != null){
